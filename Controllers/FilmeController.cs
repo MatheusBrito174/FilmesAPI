3e88b78d1752a66b6cebb63a51139a2ba2ff5773
Matheus <matheusbrito174@gmail.com>
2022-04-06 21:52:36 -0300

    Iniciando implementação da classe FilmeController

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController
+    {
+        private static List<Filme> _filmes = new List<Filme>();
+        
+        [HttpGet(Name = "GetFilmes")]
+        public IEnumerable<Filme> Get()
+        {
+            return _filmes;
+        }
+
+        [HttpPost(Name = "PostFilme")]
+        public void cadastrarFilme([FromBody] Filme filme)
+        {
+            _filmes.Add(filme);
+        }
+    }
+}
