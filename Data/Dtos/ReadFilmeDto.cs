8b0a2b29f6e6edbfa92e94088f52328626fcaea6
Matheus <matheusbrito174@gmail.com>
2022-04-11 21:55:49 -0300

    Criando DTOs

---
+++ Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required(ErrorMessage = "required")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "required")]
+        [Display(Name = "titulo")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "required")]
+        [Display(Name = "diretor")]
+        public string Diretor { get; set; }
+
+        [Display(Name = "genero")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "range")]
+        [Display(Name = "duracao")]
+        public int Duracao { get; set; }
+
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
---
+++ Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "required")]
+        [Display(Name = "titulo")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "required")]
+        [Display(Name = "diretor")]
+        public string Diretor { get; set; }
+
+        [Display(Name = "genero")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "range")]
+        [Display(Name = "duracao")]
+        public int Duracao { get; set; }
+    }
+}
